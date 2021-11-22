103129a416932100389c47ca626a924a11b9b699
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2021-11-22 14:42:23 +0100

    Rovarsprak

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // lyssna på knapptryck
 
-    button.addEventListener('click', (e) => {
+    textbox.addEventListener('change', (e) => {
         // läs in texten från textboxen
         const text = textbox.value;
         // skicka texten till funktionen för att översätta
         const translation = translate(text);
         // Hur kan du rensa textboxen?
+        textbox.value = '';
         // skriv ut översättningen i message
         display(translation, message);
     });
@@ -23,8 +24,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const translate = (text) => {
     // översätt texten till rövarspråket
-    console.log(text)
-    return text;
+    
+    let translation = ''; 
+
+    const vokaler = "a o u i e y å ä ö";
+
+    for(let i = 0; i < text.length; i++){
+        //console.log(text[i]);
+        if (vokaler.includes(text[i])) {        //(text[i] == 'a' || text[i] == 'o' || text[i] == 'u' || text[i] == 'i' || text[i] == 'e' || text[i] == 'y' || text[i] == 'å' || text[i] == 'ä' || text[i] == 'ö')
+            translation = translation + text[i];
+        } else{
+            translation = translation + text[i] + 'o' + text[i];
+        }
+    }
+
+    return translation;
 };
 
 const display = (translation, element) => {
